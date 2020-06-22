21a3b238a8eb779eb53c09286cd9143f6a52ca56
z3r0101 <ryanzkizen@gmail.com>
2020-06-23 01:43:29 +0800

    20200622 1:43

--- classes/database.php
+++ classes/database.php
@@ -19,16 +19,51 @@ class cmsDatabaseClass {
             mysqli_ssl_set($this->mysqli, NULL, NULL, $CONFIG['database'][$index]['ssl']['cert'], NULL, NULL);
             mysqli_real_connect($this->mysqli, $CONFIG['database'][$index]['host'], $CONFIG['database'][$index]['username'], $CONFIG['database'][$index]['password'], $CONFIG['database'][$index]['name'], $CONFIG['database'][$index]['port']);
         } else {
-            $this->mysqli = new mysqli($CONFIG['database'][$index]['host'], $CONFIG['database'][$index]['username'], $CONFIG['database'][$index]['password'], $CONFIG['database'][$index]['name']);
+            $this->mysqli = @new mysqli($CONFIG['database'][$index]['host'], $CONFIG['database'][$index]['username'], $CONFIG['database'][$index]['password'], $CONFIG['database'][$index]['name']);
         }
 
         if ($this->mysqli->connect_error) {
-            print pageError("Database connection failed", $this->mysqli->connect_error);
+            print pageError("Database connection failed", $this->mysqli->connect_error."<br>Please check your database configuration in ".WWWPATH."www/config.php");
             exit;
         } else {
             $cmsDBCheckData = self::select("SHOW TABLES LIKE 'cms_users'");
             if (count($cmsDBCheckData) == 0) {
-                print pageError("CMS Error", "CMS users table not found");
+
+                #Detect import
+                $arrImport = glob(WWWPATH."import/*");
+                foreach($arrImport as $Index => $File) {
+                    $tFile = basename($File);
+                    $tPathInfo = pathinfo($tFile);
+
+                    if ($tPathInfo['extension'] == 'sql') {
+                        $this->mysqli->multi_query(file_get_contents($File));
+                    }
+                    if ($tPathInfo['extension'] == 'json') {
+                        $arrJSON = json_decode(file_get_contents($File), true);
+                        foreach($arrJSON as $Index => $Data) {
+                            $upload_parent_dir = WWWPATH.'uploads/'.$Data['upload_parent_dir'];
+
+                            if (!file_exists($upload_parent_dir))
+                                mkdir($upload_parent_dir, 0777);
+
+                            if (file_exists($upload_parent_dir))
+                                mkdir($upload_parent_dir.'/'.$Data['id'], 0777);
+
+                            if (file_exists($upload_parent_dir.'/'.$Data['id'])) {
+                                mkdir($upload_parent_dir.'/'.$Data['id'].'/'.$Data['upload_container_dir'], 0777);
+                            }
+
+                            $file = fopen($upload_parent_dir.'/'.$Data['id'].'/'.$Data['upload_container_dir'].'/'.$Data['file'], "wb");
+                            fwrite($file, base64_decode($Data['data']));
+                            fclose($file);
+                        }
+                    }
+                }
+
+                $cmsRedirect = $CONFIG['website']['path'].$CONFIG['cms']['route_name']."/login";
+                header("location: {$cmsRedirect}");
+
+                #print pageError("CMS Error", "CMS users table not found");
                 exit;
             } else {
                 $arrDBErrorMsg = array();
@@ -38,21 +73,26 @@ class cmsDatabaseClass {
                 $CMS_Users_Password_Conf = '';
                 if (isset($_POST["cms-super-admin-setup"])) {
                     $CMS_Users_Email = (isset($_POST["cms-email"])) ? trim($_POST["cms-email"]) : "";
-                    $CMS_Users_Name = (isset($_POST["cms-username"])) ? trim($_POST["cms-username"]) : "";
+                    $CMS_Users_Name = $CMS_Users_Email;
                     $CMS_Users_Password = (isset($_POST["cms-password"])) ? trim($_POST["cms-password"]) : "";
                     $CMS_Users_Password_Conf = (isset($_POST["cms-password-confirm"])) ? trim($_POST["cms-password-confirm"]) : "";
+                    $CMS_Users_Name_First = (isset($_POST["cms-fname"])) ? trim($_POST["cms-fname"]) : "";
+                    $CMS_Users_Name_Last = (isset($_POST["cms-lname"])) ? trim($_POST["cms-lname"]) : "";
 
                     if ($CMS_Users_Email == '') {
                         $arrDBErrorMsg[] = "Email is required";
-                    }
-                    if ($CMS_Users_Name == '') {
-                        $arrDBErrorMsg[] = "Username is required";
                     }
                     if ($CMS_Users_Password == '') {
                         $arrDBErrorMsg[] = "Password is required";
                     }
                     if ($CMS_Users_Password_Conf == '') {
                         $arrDBErrorMsg[] = "Confirm Password is required";
+                    }
+                    if ($CMS_Users_Name_First == '') {
+                        $arrDBErrorMsg[] = "First Name is required";
+                    }
+                    if ($CMS_Users_Name_Last == '') {
+                        $arrDBErrorMsg[] = "Last Name is required";
                     }
 
                     if (count($arrDBErrorMsg) == 0) {
@@ -87,6 +127,8 @@ class cmsDatabaseClass {
                             array(
                                 'CMS_Users_Email'=>$CMS_Users_Email,
                                 'CMS_Users_Name'=>$CMS_Users_Name,
+                                'CMS_Users_Name_First'=>$CMS_Users_Name_First,
+                                'CMS_Users_Name_Last'=>$CMS_Users_Name_Last,
                                 'CMS_Users_Password'=>$tCrypt->encrypt($CMS_Users_Password),
                                 'CMS_Users_Status'=>1,
                                 'CMS_Users_Date_Created'=>date("Y-m-d H:i:s"),
@@ -100,53 +142,54 @@ class cmsDatabaseClass {
                     }
                 }
 
-//                $cmsDBCheckData = self::select("SELECT * FROM cms_users WHERE CMS_Users_Name <> 'dev'");
-//                if (count($cmsDBCheckData) == 0) {
-//                    $VENDORS_URL = $CONFIG['website']['path'].'vendors/';
-//
-//                    $htmlErrorMsg = '';
-//                    if (count($arrDBErrorMsg)>0) {
-//
-//                        $tArr = array();
-//                        foreach($arrDBErrorMsg as $Index => $Data) {
-//                            $tArr[] = '&bull; '.$Data.'<br>';
-//                        }
-//
-//                        $htmlErrorMsg = '<div style="width: 40%; padding: 0.4%; margin-bottom: 0.9%">'.implode('', $tArr).'</div>';
-//                    }
-//
-//                    $cmsEmail = ($CMS_Users_Email != '') ? 'value="'.htmlspecialchars($CMS_Users_Email, ENT_QUOTES).'"' : '';
-//                    $cmsUsername = ($CMS_Users_Name != '') ? 'value="'.htmlspecialchars($CMS_Users_Name, ENT_QUOTES).'"' : 'value="admin"';
-//                    $cmsPassword = ($CMS_Users_Password != '') ? 'value="'.htmlspecialchars($CMS_Users_Password, ENT_QUOTES).'"' : '';
-//                    $cmsPasswordConf = ($CMS_Users_Password_Conf != '') ? 'value="'.htmlspecialchars($CMS_Users_Password_Conf, ENT_QUOTES).'"' : '';
-//
-//                    $htmlData = <<<HTML
-//                        <form method="post">
-//                            {$htmlErrorMsg}
-//                            <label>Email</label><br>
-//                            <input type="text" name="cms-email" {$cmsEmail} style="width: 40%; padding: 0.4%; margin-bottom: 0.6%" required><br>
-//                            <label>Username</label><br>
-//                            <input type="text" name="cms-username" {$cmsUsername} style="width: 40%; padding: 0.4%; margin-bottom: 0.2%" placeholder="E.g. johndoe / john_doe / john-doe / john.doe" required><br>
-//                            <div style="width: 40%; padding: 0.4%; margin-bottom: 0.9%">You may change the default username <strong>admin</strong></div>
-//                            <label>Password</label><br>
-//                            <input type="password" name="cms-password" {$cmsPassword} style="width: 40%; padding: 0.4%; margin-bottom: 0.6%" required><br>
-//                            <label>Confirm Password</label><br>
-//                            <input type="password" name="cms-password-confirm" {$cmsPasswordConf} style="width: 40%; padding: 0.4%; margin-bottom: 0.2%" required><br>
-//                            <div style="width: 40%; padding: 0.4%; margin-bottom: 0.9%">
-//                                Password Rules:<br>
-//                                At least 12 characters<br>
-//                                At least one capital and one lower case letter
-//                            </div>
-//                            <button style="padding: 0.4%">Submit</button>
-//                            <input type="hidden" name="cms-super-admin-setup" value="true">
-//                        </form>
-//                        <script src="{$VENDORS_URL}jquery/jquery-3.4.1.min.js"></script>
-//HTML;
-//
-//
-//                    print pageError("Super Administrator", $htmlData);
-//                    exit;
-//                }
+                $cmsDBCheckData = self::select("SELECT * FROM cms_users WHERE CMS_Users_Name <> 'dev'");
+                if (count($cmsDBCheckData) == 0) {
+                    $VENDORS_URL = $CONFIG['website']['path'].'vendors/';
+
+                    $htmlErrorMsg = '';
+                    if (count($arrDBErrorMsg)>0) {
+
+                        $tArr = array();
+                        foreach($arrDBErrorMsg as $Index => $Data) {
+                            $tArr[] = '&bull; '.$Data.'<br>';
+                        }
+
+                        $htmlErrorMsg = '<div style="width: 40%; padding: 0.4%; margin-bottom: 0.9%">'.implode('', $tArr).'</div>';
+                    }
+
+                    $cmsEmail = ($CMS_Users_Email != '') ? 'value="'.htmlspecialchars($CMS_Users_Email, ENT_QUOTES).'"' : '';
+                    $cmsUsername = ($CMS_Users_Name != '') ? 'value="'.htmlspecialchars($CMS_Users_Name, ENT_QUOTES).'"' : 'value="admin"';
+                    $cmsPassword = ($CMS_Users_Password != '') ? 'value="'.htmlspecialchars($CMS_Users_Password, ENT_QUOTES).'"' : '';
+                    $cmsPasswordConf = ($CMS_Users_Password_Conf != '') ? 'value="'.htmlspecialchars($CMS_Users_Password_Conf, ENT_QUOTES).'"' : '';
+
+                    $htmlData = <<<HTML
+                        <form method="post">
+                            {$htmlErrorMsg}
+                            <label>Email</label><br>
+                            <input type="text" name="cms-email" {$cmsEmail} style="width: 40%; padding: 0.4%; margin-bottom: 0.6%" required><br>
+                            <label>Password</label><br>
+                            <input type="password" name="cms-password" {$cmsPassword} style="width: 40%; padding: 0.4%; margin-bottom: 0.6%" required><br>
+                            <label>Confirm Password</label><br>
+                            <input type="password" name="cms-password-confirm" {$cmsPasswordConf} style="width: 40%; padding: 0.4%; margin-bottom: 0.2%" required><br>
+                            <div style="width: 40%; padding: 0.4%; margin-bottom: 0.9%">
+                                Password Rules:<br>
+                                At least 12 characters<br>
+                                At least one capital and one lower case letter
+                            </div>
+                            <label>First Name</label><br>
+                            <input type="text" name="cms-fname" {$cmsPassword} style="width: 40%; padding: 0.4%; margin-bottom: 0.6%" required><br>
+                            <label>Last Name</label><br>
+                            <input type="text" name="cms-lname" {$cmsPassword} style="width: 40%; padding: 0.4%; margin-bottom: 0.6%" required><br>
+                            <button style="padding: 0.4%">Submit</button>
+                            <input type="hidden" name="cms-super-admin-setup" value="true">
+                        </form>
+                        <script src="{$VENDORS_URL}jquery/jquery-3.4.1.min.js"></script>
+HTML;
+
+
+                    print pageError("CMS Administrator", $htmlData);
+                    exit;
+                }
             }
         }
         $this->database = $CONFIG['database'][$index]['name'];
