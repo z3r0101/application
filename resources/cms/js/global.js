2de83d0655f769ad1b067682edea8be33b13abb7
z3r0101 <ryanzkizen@gmail.com>
2020-09-22 17:16:27 +0800

    Dev Updates

--- resources/cms/js/global.js
+++ resources/cms/js/global.js
@@ -1209,12 +1209,63 @@ function cmsAssetUpload(pObj, pId, pMode, pOption, pExt, pExt2) {
 
             cmsAssetDialog.getButton('btn-save').html('Save');
 
-            if (/^image\/\w+$/.test(file.type)) {
-                var uploadedImageURL = URL.createObjectURL(file);
-                cmsAssetLoadImage(uploadedImageURL, file.name, file.type);
-            } else {
-                cmsAssetLoadFile('', file.name, file.type);
-            }
+            var reader = new FileReader();
+            reader.addEventListener("load", function () {
+                // convert image file to base64 string
+                var arr = new Uint8Array(reader.result), i, len, length = arr.length, frames = 0;
+
+                // make sure it's a gif (GIF8)
+                if (arr[0] !== 0x47 || arr[1] !== 0x49 ||
+                    arr[2] !== 0x46 || arr[3] !== 0x38)
+                {
+                    $.event.trigger({
+                        type: "CMS_ASSET_CHECK_FILE",
+                        data: null,
+                        isGifAni: false
+                    });
+                    return;
+                }
+
+                //ported from php http://www.php.net/manual/en/function.imagecreatefromgif.php#104473
+                //an animated gif contains multiple "frames", with each frame having a
+                //header made up of:
+                // * a static 4-byte sequence (\x00\x21\xF9\x04)
+                // * 4 variable bytes
+                // * a static 2-byte sequence (\x00\x2C) (some variants may use \x00\x21 ?)
+                // We read through the file til we reach the end of the file, or we've found
+                // at least 2 frame headers
+                for (i=0, len = length - 9; i < len && frames < 2; ++i) {
+                    if (arr[i] === 0x00 && arr[i+1] === 0x21 &&
+                        arr[i+2] === 0xF9 && arr[i+3] === 0x04 &&
+                        arr[i+8] === 0x00 &&
+                        (arr[i+9] === 0x2C || arr[i+9] === 0x21))
+                    {
+                        frames++;
+                    }
+                }
+
+                $.event.trigger({
+                    type: "CMS_ASSET_CHECK_FILE",
+                    data: null,
+                    isGifAni: (frames > 0) ? true : false
+                });
+            }, false);
+            reader.readAsArrayBuffer(file);
+
+            $(document).on('CMS_ASSET_CHECK_FILE',
+                function (pObj) {
+                    if (pObj.isGifAni) {
+                        cmsAssetLoadFile('', file.name, file.type);
+                    } else {
+                        if (/^image\/\w+$/.test(file.type)) {
+                            var uploadedImageURL = URL.createObjectURL(file);
+                            cmsAssetLoadImage(uploadedImageURL, file.name, file.type);
+                        } else {
+                            cmsAssetLoadFile('', file.name, file.type);
+                        }
+                    }
+                }
+            );
         }
     } else if (pMode == 2) {
         //UPLOAD CROPPED IMAGE
