270e4ae277e7fed00c2aebae57599a111916b970
z3r0101 <ryanzkizen@gmail.com>
2020-10-19 22:11:57 +0800

    Framework updates

--- resources/cms/js/cms.ctrl.repeater.js
+++ resources/cms/js/cms.ctrl.repeater.js
@@ -284,7 +284,7 @@ function cmsFnCtrlRepeater_Update(pCtrlId, pObj) {
     }
 
     if (arrData[dataIndex]) {
-        if ($(pObj).val()) {
+        if ($(pObj)[0]) {
             arrData[dataIndex][$(pObj).attr('data-ctrl-name')] = $(pObj).val().trim();
         }
     }
